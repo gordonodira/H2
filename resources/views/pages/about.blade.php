f05d46a6a8f9e6ba1fef92df896bf3632a86914c
Gordon Odira <sellemap@gmail.com>
2018-10-16 11:28:29 -0400

    1.Laravel initial set up 2. Procfile added 3.Added include folder:( footer,header and head files) 4.layout folder (default file) 5.Routes-web.php file 6.Pages-home and about blade files

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,9 @@
+
+@extends('layouts.default')
+@section('content')
+    <body>
+    <h1>Hello World  and Welcome to my About Page</h1>
+
+    </body>
+
+@stop
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,6 @@
+
+@extends('layoutS.default')
+@section('content')
+    <h1>Hello World  and Welcome to my Home Page</h1>
+
+@stop
